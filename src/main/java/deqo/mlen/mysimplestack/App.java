d9336fc280e353ad7e476f82850aebd6e2c6aeaa
Matthieu31 <lenoirmatthieu@yahoo.fr>
2018-10-19 16:52:07 +0200

    Premier commit

---
+++ src/main/java/deqo/mlen/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.mlen.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

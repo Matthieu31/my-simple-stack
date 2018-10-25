8a64b459032687514bcda479a10ec27a7aa65323
Matthieu31 <lenoirmatthieu@yahoo.fr>
2018-10-25 14:17:20 +0200

    #2 création de la classe MySimpleStackTest

---
+++ src/test/java/deqo/mlen/mysimplestack/MySimpleStackTest.java
@@ -0,0 +1,28 @@
+package deqo.mlen.mysimplestack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MySimpleStackTest {
+
+    @Test
+    public void testIsEmpty() {
+    }
+
+    @Test
+    public void testGetSize() {
+    }
+
+    @Test
+    public void testPush() {
+    }
+
+    @Test
+    public void testPeek() {
+    }
+
+    @Test
+    public void testPop() {
+    }
+}
